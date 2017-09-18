5f4bac3f21b6386cee076f7406dc73225174d624
Riei Ishizeki <rishizek@gmail.com>
2017-09-18 16:08:16 +0900

    Move variables lane and ref_vel to outsize of h.onMessage() function

--- src/main.cpp
+++ src/main.cpp
@@ -197,13 +197,14 @@ int main() {
     map_waypoints_dy.push_back(d_y);
   }
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
-                     uWS::OpCode opCode) {
-    //start in lane 1;
-    int lane = 1;
+  //start in lane 1;
+  int lane = 1;
 
-    // Have a reference velocity to target
-    double ref_vel = 49.5; //mph
+  // Have a reference velocity to target
+  double ref_vel = 49.5; //mph
+
+  h.onMessage([&ref_vel,&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy,&lane](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+                     uWS::OpCode opCode) {
 
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
